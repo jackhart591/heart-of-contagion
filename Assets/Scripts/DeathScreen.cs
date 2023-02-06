0a14d668d96dd920fe50c67b222f5359809fcb08
Endersky78 <jackhart591@gmail.com>
2023-02-05 21:12:20 -0800

    Added some stuff

--- Assets/Scripts/DeathScreen.cs
+++ Assets/Scripts/DeathScreen.cs
@@ -8,6 +8,7 @@ public class DeathScreen : MonoBehaviour {
     float opacity = 0f;
     
     public IEnumerator TriggerDeath() {
+        Debug.Log("starting");
         if (opacity < 255f) {
             opacity += 1;
             transform.GetChild(0).GetComponent<Image>().material.color = new Color(0f, 0f, 0f, opacity);
---
+++ Assets/Scripts/endScreenTrigger.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class endScreenTrigger : MonoBehaviour
+{
+
+    public GameObject deathScreenObj;
+
+    private void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            StartCoroutine(deathScreenObj.GetComponent<DeathScreen>().TriggerDeath());
+        }
+    }
+}
